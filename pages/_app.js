0d63c7970083af15d890fbf783821116f2e32349
gabrielraymond <gabrielraymond.grd@gmail.com>
2022-07-03 15:47:49 +0700

    fixing bootstrap import

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,15 @@ import Head from "next/head";
 import Layout from "../components/layout/Layout";
 // add bootstrap css
 import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap";
+// import "bootstrap/dist/js/bootstrap";
 import "../styles/globals.css";
 import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    import("bootstrap/dist/js/bootstrap");
+    typeof document !== undefined
+      ? require("bootstrap/dist/js/bootstrap")
+      : null;
   }, []);
   return (
     <Layout>
